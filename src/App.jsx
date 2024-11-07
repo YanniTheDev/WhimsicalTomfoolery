8d24e3b92d7c8d3c23f6e95a7dbb343532a90df5
YanniTheDev <yannithedev@gmail.com>
2024-11-07 20:38:31 +1300

    here we go >:)

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 
 import './App.css'
 
+// Components
+import { AccessPage } from './Components/AccessPage.jsx';
+
 function App() {
   return (
     <>
-      Hello
+      <AccessPage />
     </>
   )
 }
---
+++ src/Components/AccessPage.jsx
@@ -0,0 +1,18 @@
+import "../Reusables.css";
+import "../ComponentsCSS/AccessPage.css";
+
+export const AccessPage = () => {
+    return (
+        <div className="access-page-container flex-c-c flex-dir-col segoe-ui">
+            <h1 className="title hack-green">Access Page</h1>
+
+            <h3 className="header hack-green">Please Enter Your <em>FULL</em> Name and Access Token</h3>
+
+            <p className="hack-green">NAME:</p>
+            <input type="text" placeholder="FULL NAME?"/>
+
+            <p className="hack-green">Access Token:</p>
+            <input type="text" placeholder="TOKEN?"/>
+        </div>
+    );
+}
